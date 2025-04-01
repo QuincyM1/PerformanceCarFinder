084bf84912ec1beeabde726064c7447be6ce7919
QuincyM1 <quincymaquincyma@gmail.com>
2025-03-31 22:21:26 -0400

    Added unit test and fixed test directory structure

---
+++ src/test/java/com/performancecarsfinder/service/CarServiceTest.java
@@ -0,0 +1,12 @@
+package com.performancecarsfinder.service;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CarServiceTest {
+
+    @Test
+    void testSimpleAddition() {
+        assertEquals(4, 2 + 2);  // Just a simple test to get started
+    }
+}
